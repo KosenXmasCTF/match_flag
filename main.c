eca5940d94d22123d70fb30f195bbfb44a23abbc
Iwancof <rock.iwancof@gmail.com>
2020-12-22 01:56:47 +0900

    Add null check

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
 
 int main() {
 	FILE *fp = fopen("./flag.txt", "r");
+	if(fp == NULL) {
+		puts("flag.txt not found!");
+		exit(0);
+	}
 	char flag[0x100];
 	fgets(flag, 0x100, fp);
 
